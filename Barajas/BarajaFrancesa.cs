f25ff28840270ac34718406f30185afcbd827a0e
msilvapasiona <106726698+msilvapasiona@users.noreply.github.com>
2022-07-01 11:40:19 +0100

    Update BarajaFrancesa.cs

--- Barajas/BarajaFrancesa.cs
+++ Barajas/BarajaFrancesa.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,10 @@ namespace Barajas
 {
     public class BarajaFrancesa:IPalosNumeros
     {
-        IDictionary<string, int> numeros = new Dictionary<string, int>();
-
         public BarajaFrancesa()
         {
             Palos = new string[] { "picas", "treboles", "corazones", "diamantes"};
-            Numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            Numeros = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
         }
         
         public string[] Palos
